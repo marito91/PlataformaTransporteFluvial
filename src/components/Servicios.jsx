004869b782330741d9e72cdcfc00576d17b3eb41
Mario Gómez <magv181091@gmail.com>
2021-11-26 21:18:05 -0500

    .

--- src/components/Servicios.jsx
+++ src/components/Servicios.jsx
@@ -10,13 +10,13 @@ export default function Servicios() {
             <section className="u-clearfix u-custom-color-2 u-section-1" id="sec-1d76">
                 <div className="u-clearfix u-sheet u-sheet-1">
                     <h2 className="u-align-center u-custom-font u-font-raleway u-text u-text-custom-color-3 u-text-default u-text-1">Servicios</h2>
-                    <p className="u-align-center u-custom-font u-font-raleway u-text u-text-2" /> Somos una empresa dedicada al transporte fluvial de mercancía en todo el territorio colombiano.<br/>
-                    <br/>Con más de 20 años de experiencia y 20 puertos a nuestra disposición, contamos con un servicio de logística implacable.<br/>
+                    <p className="u-align-center u-custom-font u-font-raleway u-text u-text-2"> Somos una empresa dedicada al transporte fluvial de mercancía en todo el territorio colombiano.<br/>
+                    <br/>Con más de 20 años de experiencia y 20 puertos a nuestra disposición, contamos con un servicio de logística implacable.</p><br/>
                     <br/>También ofrecemos logística en línea para todos nuestros clientes y usuarios externos. 
                     <span className="u-file-icon u-icon u-icon-rounded u-opacity u-opacity-40 u-palette-1-base u-spacing-10 u-icon-1">
                         <img src={heavyBox} alt="" />
                     </span>
-                    <p className="u-align-center u-custom-font u-font-raleway u-text u-text-3" /> Cuentas con toda la seguridad para transportar tus cargas y productos a través del sistema fluvial colombiano.<br/>
+                    <p className="u-align-center u-custom-font u-font-raleway u-text u-text-3"> Cuentas con toda la seguridad para transportar tus cargas y productos a través del sistema fluvial colombiano.</p><br/>
                     <br/>¡Afíliate a nuestros servicios!
                     
                 </div>
